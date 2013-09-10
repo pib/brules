7a4884ec8af6887ddeb4bc20a180ae6859839962
Paul Bonser <pib@paulbonser.com>
2013-09-10 17:14:23 -0500

    Add support for checking tag existence and attribute lengths

--- brules/helpers/html.py
+++ brules/helpers/html.py
@@ -1,23 +1,51 @@
 from ..steps import RegexFuncStep
+from ..stepset import StepSet
 
+attr = r'(?P<attr>\w+)'
+val = r'(?P<val>.+)'
 tag = r'(?P<tag>\w+)'
+name = r'(?P<name>\w+)'
+tag_attr = r'(?P<tag_attr>tag|attribute)'
 less_more = '(?P<less_more>less|more)'
 length = r'(?P<length>\d+)'
 chars_words = '(?P<chars_words>characters|words)'
 
 
-@RegexFuncStep.make(r'Does the {} tag have {} than {} {}?'.format(
-    tag, less_more, length, chars_words))
-def check_tag_length(context, args):
-    # Does the title tag have less than 60 characters?
+@RegexFuncStep.make(r'Does the page have a[n]? {} tag\?'.format(tag))
+def check_tag_exists(context, args):
+    # Does the page have a title tag?
     tag = context.etree.find('.//' + args.tag)
+    return tag is not None
+
+
+@RegexFuncStep.make(r'Does the {} {} have {} than {} {}\?'.format(
+    name, tag_attr, less_more, length, chars_words))
+def check_tag_length(context, args):
+    if args.tag_attr == 'attribute':
+        # Does the content attribute have less than 160 characters?
+        txt = context.selected_tag.attrib[args.name]
+    else:
+        # Does the title tag have less than 60 characters?
+        txt = context.etree.find('.//' + args.name).text_content()
 
     if args.chars_words == 'characters':
-        tag_length = len(tag.text)
+        tag_length = len(txt)
     elif args.chars_words == 'words':
-        tag_length = len(tag.text_content().strip().split())
+        tag_length = len(txt.strip().split())
 
     if args.less_more == 'less':
         return tag_length < int(args.length)
     elif args.less_more == 'more':
         return tag_length > int(args.length)
+
+
+@RegexFuncStep.make(r'Given the {} tag with the attribute {}={}'.format(
+    tag, attr, val))
+def given_tag_attr(context, args):
+    tag = context.etree.find('.//{}[@{}="{}"]'.format(args.tag,
+                                                      args.attr,
+                                                      args.val))
+    context.selected_tag = tag
+
+
+html_step_set = StepSet(check_tag_exists, check_tag_length, given_tag_attr)
--- tests/test_html_helpers.py
+++ tests/test_html_helpers.py
@@ -1,17 +1,39 @@
 from unittest import TestCase
 from brules import Rule
 from brules.helpers.basic import basic_step_set
-from brules.helpers import html
+from brules.helpers.html import html_step_set
 from lxml.html import fromstring
 
 
 class HtmlHelpersTest(TestCase):
     def setUp(self):
         self.rule = Rule()
-        self.rule.add_step(html.check_tag_length)
+        self.rule.add_step_set(html_step_set)
         self.rule.add_step_set(basic_step_set)
 
-    def test_check_tag_length_lxml(self):
+    def test_check_tag_exists(self):
+        html1 = """
+            <html><head><title>This is the title, yo.</title></head></html>
+            """
+        html2 = """<html><head></head><body><h1>YO</h1></body></html>"""
+        rule_txt = """\
+            Does the page have a title tag?
+            If so, then set title to: yes
+            If not, then set title to: no
+
+            Does the page have an h1 tag?
+            If so, then set h1 to: yes
+            If not, then set h1 to: no
+            """
+        self.rule.parse(rule_txt)
+        self.rule.run(etree=fromstring(html1))
+        self.assertEquals(self.rule.context.title, 'yes')
+        self.assertEquals(self.rule.context.h1, 'no')
+        self.rule.run(etree=fromstring(html2))
+        self.assertEquals(self.rule.context.title, 'no')
+        self.assertEquals(self.rule.context.h1, 'yes')
+
+    def test_check_tag_length(self):
         html_txt = """
             <html>
               <head>
@@ -50,3 +72,14 @@ class HtmlHelpersTest(TestCase):
         self.rule.run(etree=fromstring(html_txt))
         self.assertEqual(self.rule.context.answers,
                          ['yes', 'yes', 'yes', 'no', 'no', 'no'])
+
+    def test_given_tag_with_attr(self):
+        html = '<html><a></a><a href="foo.html">Foo</a></html>'
+        rule = ('Given the a tag with the attribute href=foo.html\n'
+                'Does the href attribute have less than 20 characters?')
+
+        self.rule.parse(rule)
+        print(self.rule.steps)
+        self.rule.run(etree=fromstring(html))
+        self.assertEquals(self.rule.context.selected_tag.tag, 'a')
+        self.assertEquals(self.rule.context.selected_tag.text, 'Foo')
